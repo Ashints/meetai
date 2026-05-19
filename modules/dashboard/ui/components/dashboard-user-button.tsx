e46a4d87f5aee4bd451c1ee071c7437fd63b5c5d
Ashints <ashinsali77@gmail.com>
2026-05-19 15:35:30 +0530

    07: dashboard navbar

--- modules/dashboard/ui/components/dashboard-user-button.tsx
+++ modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,6 +1,7 @@
 import { authClient } from "@/lib/auth-clients";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { useRouter } from "next/navigation";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { ChevronsDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
@@ -12,8 +13,21 @@ import { DropdownMenu,
     DropdownMenuTrigger,
  } from "@/components/ui/dropdown-menu";
 
+  import {
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+
+
 export const DashboardUserButton=()=>{
     const router=useRouter();
+    const isMobile=useIsMobile();
     const{data, isPending}=authClient.useSession();
     if(isPending || !data?.user){
             return null;
@@ -27,10 +41,60 @@ export const DashboardUserButton=()=>{
             }
         })
     }
+    if(isMobile){
+        return(
+            <Drawer>
+            <DrawerTrigger className="rounded-lg border border/10 p-3 w-full flex items-center justify-between bg-white/5
+             hover:bg-white/10 overflow-hidden gap-x-2">
+                {data.user.image ?(
+            <Avatar>         
+            <AvatarImage src={data.user.image}/>
+            </Avatar>
+            ) :(
+                <GeneratedAvatar
+                 seed={data.user.name}
+                 variant="initials"
+                 className="=size-9 mr-3"
+                />
+            )}    
+            <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+                <p className="text-sm truncate w-full ">
+                    {data.user.name}
+                </p>
+                <p className="text-xs truncate w-full">
+                    {data.user.email}
+                </p>
+                </div>   
+                <ChevronsDownIcon className="size-4 shrink-0"/>     
+            </DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>
+                        {data.user.name}
+                    </DrawerTitle>
+                    <DrawerDescription>
+                        {data.user.email}
+                    </DrawerDescription>
+                </DrawerHeader>
+                <DrawerFooter>
+                    <Button variant="outline" onClick={()=>{}}>
+                        <CreditCardIcon className="size-4 text-black"/>
+                        Billing
+                    </Button>
+                    <Button variant="outline" onClick={onLogout}>
+                        <LogOutIcon className="size-4 text-black"/>
+                        Logout
+                    </Button>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+
+        )        
+    }
     return(
         <DropdownMenu>
             <DropdownMenuTrigger className="rounded-lg border border/10 p-3 w-full flex items-center justify-between bg-white/5
-             hover:bg-white/10 overflow-hidden">
+             hover:bg-white/10 overflow-hidden gap-x-2">
             {data.user.image ?(
             <Avatar>         
             <AvatarImage src={data.user.image}/>
