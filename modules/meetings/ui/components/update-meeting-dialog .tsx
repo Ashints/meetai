a650292509016623232605dbfa296f2829421f08
Ashints <ashinsali77@gmail.com>
2026-06-07 13:48:25 +0530

    20: meeting page

--- modules/meetings/ui/components/update-meeting-dialog .tsx
+++ modules/meetings/ui/components/update-meeting-dialog .tsx
@@ -1,29 +1,28 @@
 import { ResponsiveDialog } from "@/components/responsive-dialog";
 import { MeetingForm } from "./meeting-form";
-import { useRouter } from "next/navigation";
+import { MeetingGetOne } from "../../types";
 
-interface NewMeetingDialogProps{
+interface UpdateMeetingDialogProps{
     open:boolean;
     onOpenChange:(open:boolean)=>void
+    initialvalues: MeetingGetOne;
 };
 
-export const NewMeetingDialog = ({
+export const UpdateMeetingDialog = ({
     open,
     onOpenChange,
-}:NewMeetingDialogProps) =>{
-    const router = useRouter();
+    initialvalues,
+}:UpdateMeetingDialogProps) =>{
     return(
         <ResponsiveDialog 
-        title="New Meeting"
-        description="Create a new Meeting"
+        title="Edit Meeting"
+        description="Edit the meeting details"
         open={open}
         onOpenChange={onOpenChange}>
             <MeetingForm
-            onSuccess={(id)=>{
-                onOpenChange(false);
-                router.push(`/meetings/${id}`)
-            }}
+            onSuccess={()=>{onOpenChange(false);}}
             onCancel={()=>onOpenChange(false)}
+            initialValues={initialvalues}
             />
         </ResponsiveDialog>
     )
